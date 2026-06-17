454bbae2e73cb083384987a64a67921b580599d6
Brian80053 <imiyeong743@gmail.com>
2026-06-17 10:35:42 +0900

    Add files via upload

--- project_Seaborgium/main.c
+++ project_Seaborgium/main.c
@@ -22,8 +22,9 @@ int main(){
     int mines[mine_num];
     int num[size_x][size_y];
     int flags[size_x][size_y];
+    int visit[size_x][size_y];
     init(map,mines,size_x,size_y,mine_num);
-    check_number(map,num,flags,size_x,size_y,mine_num);
+    check_number(map,num,flags,size_x,size_y,mine_num,visit);
     int i,j;
     int turn = 0;
     int x,y;
@@ -33,7 +34,7 @@ int main(){
         for(i=1; i<size_x; i++){
             for(j=1; j<size_y; j++){
                 if(map[i][j]==9){
-                    printf("F"); //임시 값
+                    printf("\u2691"); //임시 값
                 }
                 else if(map[i][j]>=2){
                     printf("\u25A0");
@@ -63,6 +64,7 @@ int main(){
             flag = false;
             //지뢰면 처음 빼고 터트리기
             if(turn==0 && map[x][y]==3){
+                
                 continue;
             }
             else if(map[x][y]==3){
@@ -71,7 +73,13 @@ int main(){
             }
             //빈칸이면 DFS 탐색으로 꺼내기
             else if(map[x][y]==2){
-
+                printf("test");
+                search_out(x,y,visit,map,num,size_x,size_y);
+                for(i=1; i<size_x; i++){
+                    for(j=1; j<size_y; j++){
+                        visit[i][j]=0;
+                    }
+                }
             }
             //주변 밝히기 -> 필요한 부분이 플래그 처리 되어있으면 넘기고 아니면 버려라
             else if(map[x][y]==0){
@@ -99,7 +107,37 @@ int main(){
         turn++;
     }
 }
-void check_number(int map[][31],int num[][31],int flags[][31],int size_x,int size_y,int mine_num){
+void search_out(int x,int y,int visit[][31],int map[][31],int num[][31],int size_x,int size_y){
+    int i,j;
+    int dist[8][2]={{1,0},{1,-1},{1,1},{0,1},{0,-1},{-1,0},{-1,1},{-1,-1}};
+    int nx,ny;
+    //일단 방문한 채로
+    if(visit[i][j]==1){
+        return;
+    }
+    visit[i][j]=1;
+    if(map[i][j]==3){
+        return;
+    }
+    if(map[i][j]==2 && num[i][j]!=0){
+        map[i][j]-=2;
+        return;
+    }
+    else{
+        for(i=0; i<8; i++){
+            nx = x+dist[i][0];
+            ny = y+dist[i][1];
+            if(nx<1 || nx >size_x-1 || ny<1 || ny>size_y-1){
+                continue;
+            }
+            if(visit[nx][ny]==1){
+                continue;
+            }
+            search_out(nx,ny,visit,map,num,size_x,size_y);
+        }
+    }
+}
+void check_number(int map[][31],int num[][31],int flags[][31],int size_x,int size_y,int mine_num,int visit[][31]){
     int i;
     int j;
     int k;
@@ -109,7 +147,8 @@ void check_number(int map[][31],int num[][31],int flags[][31],int size_x,int siz
         for(j=1; j<size_y; j++){
             flags[i][j]=0;
             num[i][j]=0;
-        }
+            visit[i][j]=0; 
+        }  
     }
     for(i=1; i<size_x; i++){
         for(j=1; j<size_y; j++){
@@ -161,13 +200,9 @@ void init(int map[][31],int mines[],int size_x,int size_y,int mine_num){
         }
         map[x][y]=1;
     }
-    int temp=0;
     for(i=1; i<size_x; i++){
         for(j=1; j<size_y; j++){
             map[i][j]+=2;
-            if(map[i][j]==3){
-                temp++;
-            }
         }
     }
 }
--- project_Seaborgium/main.h
+++ project_Seaborgium/main.h
@@ -1,2 +1,3 @@
 void init(int map[][31],int mines[],int size_x,int size_y,int mine_num);
-void check_number(int map[][31],int num[][31],int flags[][31],int size_x,int size_y,int mine_num);
+void check_number(int map[][31],int num[][31],int flags[][31],int size_x,int size_y,int mine_num,int visit[][31]);
+void search_out(int x,int y,int visit[][31],int map[][31],int num[][31],int size_x,int size_y);
